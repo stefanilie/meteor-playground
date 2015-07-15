76a0e7bd931e4f00eab328e2872cdea6a287e0b1
Ilie <stefan.ilie@rootpm4045029a.net.work>
2015-07-15 12:56:40 +0300

    added buttons and textfield

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -1,11 +1,25 @@
 // simple-todos.js
+Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
-    ]
+    tasks: function(){
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function (event){
+      event.preventDefault();
+
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+
+      event.target.text.value = "";
+    }
   });
 }
