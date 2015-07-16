ddd380fe3a4e4491f1b66adc4053c2f4111f9201
Ilie <stefan.ilie@rootpm4045029a.net.work>
2015-07-16 11:28:15 +0300

    add events listeners for checkbozes and remove buttons

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -22,4 +22,20 @@ if (Meteor.isClient) {
       event.target.text.value = "";
     }
   });
+
+  Template.task.events({
+    "click .toggle-checked": function() {
+
+      //this updates the item foud at _id.
+      //every document has it's id
+      Tasks.update(this._id, {
+        //.update has 2 params: first to identify a subset and
+        //the second to secify what should be done to thos docs.
+        $set: {checked: ! this.checked}
+      });
+    },
+    "click .delete": function() {
+      Tasks.remove(this._id);
+    }
+  });
 }
