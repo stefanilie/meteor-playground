6f71959b8433a69b9068559f4e31093a09d4e649
Stefan <ilie.stefan92@yahoo.com>
2015-07-28 11:48:59 +0300

    add twitter integration.

--- licenta/client/licenta.js
+++ licenta/client/licenta.js
@@ -25,9 +25,12 @@ Template.body.events({
     var userName = ''
     if (Meteor.user().services.facebook) {
       userName = Meteor.user().services.facebook.name
-    } else {
+    } else if (Meteor.user().services.instagram) {
       userName = Meteor.user().services.instagram.username
       accessToken = Meteor.user().services.instagram.accessToken
+    } else if (Meteor.user().services.twitter) {
+      userName = Meteor.user().services.twitter.screenName
+    // accessToken = Meteor.user().services.twitter.
     }
 
     Searches.insert({
@@ -35,32 +38,41 @@ Template.body.events({
       user: userName,
       createdAt: new Date()
     })
-
+    // 1P5P6iQfU5Oo7APMXlht9K6eC:3q3QME7rlcs9Pxk3kX1OUKBddmY21RcSdo9p4yed0cUW9ihcZP
+    // MVA1UDZpUWZVNU9vN0FQTVhsaHQ5SzZlQzozcTNRTUU3cmxjczlQeGsza1gxT1VLQmRkbVkyMVJjU2RvOXA0eWVkMGNVVzlpaGNaUA==
+    // api.twitter.com/oauth2/token
+    // TODO: Refactor the following three methods into one beautiful method.
     // Meteor.call("getRelatedTags", text, accessToken, function(err, results) {
     //    console.log(JSON.parse(results.content)['data'])
     // })
-    Meteor.call('getPosts', text, accessToken, function (err, results) {
-      var response = JSON.parse(results.content)['data']
-      //console.log(response)
-      // var baseUrl = "https://instagr.am/p/"
-      var arrayOfLinks = []
-      // var arrayOfEmbed = []
-      for (var i = 0; i < response.length; i++) {
-        var toAdd = response[i]['link']
-        var start = toAdd.length - 11
-        toAdd = toAdd.slice(start)
-        toAdd = toAdd.substring(0, toAdd.length - 1)
-        arrayOfLinks.push(toAdd)
+    // Meteor.call('getPosts', text, accessToken, function (err, results) {
+    //   var response = JSON.parse(results.content)['data']
+    //   //console.log(response)
+    //   // var baseUrl = "https://instagr.am/p/"
+    //   var arrayOfLinks = []
+    //   // var arrayOfEmbed = []
+    //   for (var i = 0; i < response.length; i++) {
+    //     var toAdd = response[i]['link']
+    //     var start = toAdd.length - 11
+    //     toAdd = toAdd.slice(start)
+    //     toAdd = toAdd.substring(0, toAdd.length - 1)
+    //     arrayOfLinks.push(toAdd)
+    //   }
+    //   // TODO: merge in one bit for
+    //   Meteor.call('getEmbed', arrayOfLinks, function (err, result) {
+    //     //console.log(embededPosts)
+    //     arrayOfLinks = []
+    //     for (var i = 0; i < result.length; i++) {
+    //       arrayOfLinks.push(result[i]['html'])
+    //     }
+    //   })
+    //   console.log(arrayOfLinks)
+    // })
+
+    Meteor.call('tweeterSearch', '#' + text, function (err, result) {
+      if (!err){
+        console.log(result);
       }
-      // TODO: merge in one bit for
-      Meteor.call('getEmbed', arrayOfLinks, function (err, result) {
-        //console.log(embededPosts)
-        arrayOfLinks = [];
-        for (var i = 0; i < result.length; i++) {
-          arrayOfLinks.push(result[i]['html']);
-        }
-      })
-      console.log(arrayOfLinks)
     })
 
     event.target.text.value = ''
--- licenta/server/server.js
+++ licenta/server/server.js
@@ -1,4 +1,5 @@
 Searches = new Mongo.Collection('searches')
+var twitter = new TwitterApi()
 // 42217566.5925b3e.43cbb5c3dd1f441eb2fa1e8d7eaa8217
 Meteor.methods({
   'getRelatedTags': function (text, accessToken) {
@@ -35,7 +36,7 @@ Meteor.methods({
       var arrEmbeded = []
       var url = 'http://api.instagram.com/oembed?url='
       for (var i = 0; i < arrayOfLinks.length; i++) {
-        //console.log(url + defaultLink + arrayOfLinks[i])
+        // console.log(url + defaultLink + arrayOfLinks[i])
         var result = HTTP.call('GET', url + defaultLink + arrayOfLinks[i])
         arrEmbeded.push(result)
       }
@@ -43,5 +44,37 @@ Meteor.methods({
     } catch (e) {
       return null
     }
+  },
+  'tweeterSearch': function (term) {
+    // try{
+    //   console.log(encodeURIComponent(term))
+    //   var result = HTTP.call("GET", "https://api.twitter.com/1.1/search/tweets.json?q="+term)
+    //   //var url  = '?q=%23freebandnames&since_id=24012619984051000&max_id=250126199840518145&result_type=mixed&count=4'
+    //   return result
+    // } catch (e){
+    //   return null
+    // }
+    try {
+      var arrOfIDs = []
+      var result = []
+      var id
+      var array = twitter.search(term)
+      var content = JSON.parse(array['content'])
+      content = content['statuses']
+      // console.log(content['statuses'])
+      // array = array['statuses']
+      for (var i = 0; i < content.length; i++) {
+        id = content[i]['id_str']
+        var rawEmbed = twitter.callAsApp('GET', 'statuses/oembed.json', {id: id})
+        rawEmbed = JSON.parse(rawEmbed['content']);
+        result.push(rawEmbed['html']);
+      }
+      console.log(result);
+      return result;
+    } catch(e) {
+      console.log(e)
+      return null
+    }
+
   }
 })
