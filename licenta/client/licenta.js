2202462757f74936ff63678bb5cc426db03f79e6
Stefan <ilie.stefan92@yahoo.com>
2015-08-07 01:35:52 +0300

    add local db

--- licenta/client/licenta.js
+++ licenta/client/licenta.js
@@ -1,68 +1,77 @@
 Searches = new Mongo.Collection('searches')
-Tweets = new Mongo.Collection('tweets')
-Posts = new Mongo.Collection('posts')
-//Meteor.subscribe('tweetSearch')
-// Meteor.loginWithInstagram(function (err, res) {
-//   if (err !== undefined)
-//     console.log('sucess ' + res)
-//   else
-//     console.log('login failed ' + err)
-// })
+  // Tweets = new Mongo.Collection('tweets')
+Tweets = new Mongo.Collection(null)
+  // Posts = new Mongo.Collection('posts')
+Posts = new Mongo.Collection(null)
+
+Meteor.subscribe('tweetPublication')
+  // Meteor.loginWithInstagram(function (err, res) {
+  //   if (err !== undefined)
+  //     console.log('sucess ' + res)
+  //   else
+  //     console.log('login failed ' + err)
+  // })
 Template.body.helpers({
-  searches: function () {
+  searches: function() {
     return Searches.find()
   },
-  tweets: function () {
+  tweets: function() {
     return Tweets.find()
   },
-  posts: function(){
+  posts: function() {
     return Posts.find()
   }
 })
 
 Template.body.events({
-  'submit .searched': function (event) {
+  'submit .searched': function(event) {
     event.preventDefault()
 
     var text = event.target.text.value
     var accessToken = ''
     var userName = ''
-    if (Meteor.user().services.facebook) {
-      userName = Meteor.user().services.facebook.name
-    } else if (Meteor.user().services.instagram) {
-      userName = Meteor.user().services.instagram.username
-      accessToken = Meteor.user().services.instagram.accessToken
-    } else if (Meteor.user().services.twitter) {
-      userName = Meteor.user().services.twitter.screenName
-    // accessToken = Meteor.user().services.twitter.
-    }
-
-    // Tweets.remove({})
-    console.log("ciorba");
-    Searches.insert({
-      text: text,
-      user: userName,
-      createdAt: new Date()
-    })
-    // TODO: Refactor the following three methods into one beautiful method.
-    // Meteor.call("getRelatedTags", text, accessToken, function(err, results) {
-    //    console.log(JSON.parse(results.content)['data'])
-    // })
-    Meteor.call('getPosts', text, accessToken, function (err, result) {
+      // if (Meteor.user().services.facebook) {
+      //   userName = Meteor.user().services.facebook.name
+     if (Meteor.user().services.instagram) {
+        userName = Meteor.user().services.instagram.username
+        accessToken = Meteor.user().services.instagram.accessToken
+      }
+      // } else if (Meteor.user().services.twitter) {
+      //   userName = Meteor.user().services.twitter.screenName
+      // // accessToken = Meteor.user().services.twitter.
+      // }
+      //
+      // // Tweets.remove({})
+      // Searches.insert({
+      //   text: text,
+      //   user: userName,
+      //   createdAt: new Date()
+      // })
+      // TODO: Refactor the following three methods into one beautiful method.
+      // Meteor.call("getRelatedTags", text, accessToken, function(err, results) {
+      //    console.log(JSON.parse(results.content)['data'])
+      // })
+    Meteor.call('getPosts', text, accessToken, function(err, result) {
       console.log(result)
       for (var i = 0; i < result.length; i++) {
         Posts.insert({
-          query: text,
-          html: result[i]
-        })
-      // arrayOfLinks.push(arrEmbeded[i]['html'])
+            query: text,
+            html: result[i]
+          })
+          // arrayOfLinks.push(arrEmbeded[i]['html'])
       }
     })
-    // Meteor.call('tweeterSearch', '#' + text, function (err, results) {
-    //   if (!err){
-    //     console.log(results)
-    //   }
-    // })
+    Meteor.call('tweeterSearch', text, function(err, results) {
+      if (!err) {
+        // console.log(results)
+        for (var i = 0; i < results.length; i++) {
+          console.log(results[i])
+          Tweets.insert({
+            html: results[i]
+          })
+        }
+      }
+    })
 
     event.target.text.value = ''
   }
--- licenta/server/server.js
+++ licenta/server/server.js
@@ -1,109 +1,120 @@
 Searches = new Mongo.Collection('searches')
-Tweets = new Mongo.Collection('tweets')
+// Tweets = new Mongo.Collection('tweets')\
 Posts = new Mongo.Collection('posts')
 var twitter = new TwitterApi()
-// 42217566.5925b3e.43cbb5c3dd1f441eb2fa1e8d7eaa8217
+  // 42217566.5925b3e.43cbb5c3dd1f441eb2fa1e8d7eaa8217
 Meteor.methods({
-  'getRelatedTags': function (text, accessToken) {
+  'getRelatedTags': function(text, accessToken) {
     check(text, String)
     check(accessToken, String)
 
     try {
-      var result = HTTP.call('GET', 'https://api.instagram.com/v1/tags/search',
-        {params: {q: text, access_token: accessToken}}
-      )
-      // console.log(result)
+      var result = HTTP.call('GET', 'https://api.instagram.com/v1/tags/search', {
+          params: {
+            q: text,
+            access_token: accessToken
+          }
+        })
+        // console.log(result)
       return result
     } catch (e) {
       return null
     }
   },
 
-  'getPosts': function (text, accessToken) {
+  'getPosts': function(text, accessToken) {
     check(text, String)
     check(accessToken, String)
 
     try {
+      console.log("entering getPosts...");
       var url = 'https://api.instagram.com/v1/tags/' + text + '/media/recent?access_token=+' + accessToken
       var result = HTTP.call('GET', url)
-      // console.log(result)
-
+      console.log("searched for "+text+".\n.\n.\n.\n.\n.\n.\n.\n");
+      console.log(result);
       // parsing the result so that we can call for the htmls
       var response = JSON.parse(result.content)['data']
 
       // getting the ids of the posts
       var arrayOfLinks = []
-      // var arrayOfEmbed = []
+        // var arrayOfEmbed = []
       for (var i = 0; i < response.length; i++) {
         var toAdd = response[i]['link']
         var start = toAdd.length - 11
         toAdd = toAdd.slice(start)
         toAdd = toAdd.substring(0, toAdd.length - 1)
         arrayOfLinks.push(toAdd)
+        console.log("added link: "+toAdd);
       }
 
       // getting the embeded posts
-      console.log('am intrat in getEmbed')
-      // TODO: Add if clause if post is picture or if post is video
+        // TODO: Add if clause if post is picture or if post is video
       var defaultLink = 'http://instagr.am/p/'
       var result = []
       var url = 'http://api.instagram.com/oembed?url='
       for (var i = 0; i < arrayOfLinks.length; i++) {
         // console.log(url + defaultLink + arrayOfLinks[i])
         var answer = HTTP.call('GET', url + defaultLink + arrayOfLinks[i])
+        console.log("got instagram embed codes...");
         answer = JSON.parse(answer['content'])['html']
         result.push(answer)
       }
       return result
     } catch (e) {
+      console.log(e);
       return null
     }
   },
 
-  'tweeterSearch': function (term) {
+  // TODO: refactor search so that it calls it as an app
+  // TODO: make validation for tweets if it
+  //alreadyexists not to be shown
+  'tweeterSearch': function(term) {
     try {
-      var arrOfIDs = []
+      console.log("entering tweeterSearch...");
+      var connections = {}
       var results = []
       var id
       var array = twitter.search(term)
+      console.log("searched for "+term);
       var content = JSON.parse(array['content'])
       content = content['statuses']
-      // console.log(content['statuses'])
-      // array = array['statuses']
-      console.log("sunt in twitter");
+
       for (var i = 0; i < content.length; i++) {
         id = content[i]['id_str']
-        var rawEmbed = twitter.callAsApp('GET', 'statuses/oembed.json', {id: id})
-        rawEmbed = JSON.parse(rawEmbed['content'])
-        Tweets.insert({
-          searched: term,
-          html: rawEmbed['html']
+        var rawEmbed = twitter.callAsApp('GET', 'statuses/oembed.json', {
+          id: id
         })
+        console.log("got embed codes...");
+        rawEmbed = JSON.parse(rawEmbed['content'])
+          // Tweets.insert({
+          //   searched: term,
+          //   html: rawEmbed['html']
+          // })
+
+        results.push(rawEmbed['html'])
+      }
 
       // TODO: Find a way to make this shite work
       // var tweet = {
       //   searched: term,
-      //   hmtl: rawEmbed['html']
+      //   data: results
       // }
-      // Meteor.publish('tweetSearch', function () {
+      // Meteor.publish('tweetPublication', function() {
       //   var init = true
-      //   var self = this
-      //   _.each(results, function (result) {
-      //     if (init) {
-      //       self.added('tweets', Random.id(), tweet)
-      //       init = false
-      //     } else {
-      //       self.changed('tweets', Random.id(), tweet)
-      //     }
+      //   var subscription = this
+      //
+      //   connections[subscription._session.id] = subscription
+      //
+      //   subscription.added('tweets', Random.id(), tweet)
+      //
+      //   subscription.onStop(function(){
+      //     delete connections[subscription._session.id]
       //   })
-      //   self.ready()
-      //   init = false
       // })
-      // results.push({html: rawEmbed['html']})
-      }
       console.log(results)
       return results
-    } catch(e) {
+    } catch (e) {
       console.log(e)
       return null
     }
