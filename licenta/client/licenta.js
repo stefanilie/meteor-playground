2711ac0b3be9a3158b762fe2dfddc82d028582f6
Stefan <ilie.stefan92@yahoo.com>
2015-08-10 10:08:08 +0300

    add sort for searched content

--- licenta/client/licenta.js
+++ licenta/client/licenta.js
@@ -5,15 +5,17 @@ Tweets = new Mongo.Collection(null)
 Posts = new Mongo.Collection(null)
 Relateds = new Mongo.Collection(null)
 
-  // Meteor.loginWithInstagram(function (err, res) {
-  //   if (err !== undefined)
-  //     console.log('sucess ' + res)
-  //   else
-  //     console.log('login failed ' + err)
-  // })
+// Meteor.loginWithInstagram(function (err, res) {
+//   if (err !== undefined)
+//     console.log('sucess ' + res)
+//   else
+//     console.log('login failed ' + err)
+// })
 Template.body.helpers({
   searches: function() {
-    return Searches.find()
+    return Searches.find(
+      {}, {sort: {count: -1}}
+    )
   },
   tweets: function() {
     return Tweets.find()
@@ -38,6 +40,8 @@ Template.body.events({
     if (Meteor.user().services.instagram) {
       userName = Meteor.user().services.instagram.username
       accessToken = Meteor.user().services.instagram.accessToken
+    } else {
+      confirm("Please login with instagram!")
     }
     // } else if (Meteor.user().services.twitter) {
     //   userName = Meteor.user().services.twitter.screenName
@@ -45,52 +49,69 @@ Template.body.events({
     // }
     //
     // // Tweets.remove({})
-    Searches.insert({
+    if (Searches.findOne({
+        text: text
+      })) {
+      var object = Searches.findOne({
+        text: text
+      })
+      var count = object['count'] + 1
+      console.log(object)
+      Searches.update({
+        _id: object['_id']
+      }, {
         text: text,
         user: userName,
-        createdAt: new Date()
+        count: count
       })
-      // TODO: Refactor the following three methods into one beautiful method.
-    Meteor.call("getRelatedTags", text, accessToken, function(err, results) {
-      console.log(results)
-      if (Relateds.findOne({})) {
-        Relateds.remove({})
-      }
-      for (var i = 0; i < results.length; i++) {
-        Relateds.insert({
-          hashtag: "#"+results[i]['name']
-        });
-      }
-    })
-
-    Meteor.call('getPosts', text, accessToken, function(err, result) {
-      console.log(result)
-      if (Posts.findOne({})) {
-        Posts.remove({})
-      }
-      for (var i = 0; i < result.length; i++) {
-        Posts.insert({
-            query: text,
-            html: result[i]
-          })
-          // arrayOfLinks.push(arrEmbeded[i]['html'])
-      }
-    })
-
-    Meteor.call('tweeterSearch', text, function(err, results) {
-      if (!err) {
-        // console.log(results)
-        if (Tweets.findOne({})) {
-          Tweets.remove({})
-        }
-        for (var i = 0; i < results.length; i++) {
-          console.log(results[i])
-          Tweets.insert({
-            html: results[i]
-          })
-        }
-      }
-    })
+    } else {
+      Searches.insert({
+        text: text,
+        user: userName,
+        count: 0
+      })
+    }
+    // TODO: Refactor the following three methods into one beautiful method.
+    // Meteor.call("getRelatedTags", text, accessToken, function(err, results) {
+    //   console.log(results)
+    //   if (Relateds.findOne({})) {
+    //     Relateds.remove({})
+    //   }
+    //   for (var i = 0; i < results.length; i++) {
+    //     Relateds.insert({
+    //       hashtag: "#"+results[i]['name']
+    //     });
+    //   }
+    // })
+    //
+    // Meteor.call('getPosts', text, accessToken, function(err, result) {
+    //   console.log(result)
+    //   if (Posts.findOne({})) {
+    //     Posts.remove({})
+    //   }
+    //   for (var i = 0; i < result.length; i++) {
+    //     Posts.insert({
+    //         query: text,
+    //         html: result[i]
+    //       })
+    //       // arrayOfLinks.push(arrEmbeded[i]['html'])
+    //   }
+    // })
+    //
+    // Meteor.call('tweeterSearch', text, function(err, results) {
+    //   if (!err) {
+    //     // console.log(results)
+    //     if (Tweets.findOne({})) {
+    //       Tweets.remove({})
+    //     }
+    //     for (var i = 0; i < results.length; i++) {
+    //       console.log(results[i])
+    //       Tweets.insert({
+    //         html: results[i]
+    //       })
+    //     }
+    //   }
+    // })
 
     event.target.text.value = ''
   }
