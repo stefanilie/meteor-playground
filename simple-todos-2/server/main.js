aa9fd0fb5c20f9967a20cb3a9f6e4d7b568b7d30
Stefan <ilie.stefan92@yahoo.com>
2016-07-26 17:47:20 +0300

    storing data in a collection

--- simple-todos-2/server/main.js
+++ simple-todos-2/server/main.js
@@ -1,5 +1,2 @@
-import { Meteor } from 'meteor/meteor';
-
-Meteor.startup(() => {
-  // code to run on server at startup
-});
+// Load tasks collection on the server
+import '../imports/api/tasks.js'
