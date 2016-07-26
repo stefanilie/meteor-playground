61483f2aedbbd3bee02b3824c30694a6ad5524e5
Stefan <ilie.stefan92@yahoo.com>
2016-07-26 18:02:02 +0300

    Adding tasks with a form

--- simple-todos-2/imports/ui/body.js
+++ simple-todos-2/imports/ui/body.js
@@ -6,6 +6,29 @@ import './body.html';
 
 Template.body.helpers({
   tasks(){
-    return Tasks.find({});
+    return Tasks.find({}, { sort: { createdAt: -1}});
+  }
+});
+
+
+Template.body.events({
+  'submit .new-task'(event) {
+    // Prevent default browser from submit
+    event.preventDefault();
+
+    console.log(event);
+
+    // Get value from form element
+    const target = event.target;
+    const text = target.text.value;
+
+    // Insert a task into the collection
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    target.text.value = '';
   }
 });
