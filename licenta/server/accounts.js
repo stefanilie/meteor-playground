6acb8eff86e17c80ec7a1127c100a50ebe3f2d66
Stefan <ilie.stefan92@yahoo.com>
2016-07-26 16:38:12 +0300

    started simple-todos-2, learning about new meteor 1.3 features

--- licenta/server/accounts.js
+++ licenta/server/accounts.js
@@ -16,7 +16,3 @@ ServiceConfiguration.configurations.insert({
   secret: "3q3QME7rlcs9Pxk3kX1OUKBddmY21RcSdo9p4yed0cUW9ihcZP",
   loginStyle: "popup"
 })
-
-//https://api.instagram.com/oauth/authorize/?client_id=881bb81f9c8049c2b7d1618df9f94ce0&redirect_uri=http://localhost:8080/_oauth/instagram&response_type=token
-
-//42217566.881bb81.c536be49151d4ce7b4723f332e6559f2
